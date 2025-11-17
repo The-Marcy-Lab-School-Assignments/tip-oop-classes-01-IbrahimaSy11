d4b71b6f3def5c3df2dc7b97c799f19ff5a2615a
ibrahimasy11 <ibhmsy11@gmail.com>
2025-11-17 10:01:04 -0500

    complete

--- index.js
+++ index.js
@@ -5,8 +5,56 @@
  */
 
 class Task {
- //write your code here
- 
+    //write your code here
+
+    static allTasks = [];
+
+    #completed = false;
+    #minutesSpent = 0;
+
+    constructor(title, priority) {
+        this.title = title;
+        this.priority = priority;
+        Task.allTasks.push(this);
+    }
+
+    get completedStatus() {
+        return this.#completed;
+    }
+
+    get timeSpent() {
+        return this.#minutesSpent;
+    }
+
+    workOn(minutes) {
+        const updated = this.#minutesSpent + minutes;
+        this.#minutesSpent = updated;
+        console.log(`Worked on ${this.title} for ${minutes} minutes. Total time: ${updated} minutes`);
+    }
+
+    complete() {
+        if (!this.#completed) {
+            this.#completed = true;
+        }
+        console.log(`${this.title} has been completed!`);
+    }
+
+    isComplete() {
+        return this.#completed;
+    }
+
+    static getTotalTasks() {
+        return this.allTasks.length;
+    }
+
+    static findByTitle(title) {
+        for (const task of this.allTasks) {
+            if (task.title === title) {
+                return task;
+            }
+        }
+        return undefined;
+    }
 }
 
 //Test class with examples below:
